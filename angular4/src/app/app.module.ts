b498ca958719081e59f03e3a742cd7dba9486dc2
msynk <msynk@outlook.com>
2017-08-17 17:02:30 +0430

    added starting components

--- angular4/src/app/app.module.ts
+++ angular4/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    AboutUsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
